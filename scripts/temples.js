62f4e6afde630b3ee9e929c117175518c499bc4c
king-nika <aojo3@byupathway.edu>
2025-03-11 17:29:27 +0100

    performance done

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,13 +1,8 @@
-const currentYear = document.getElementById("currentYear");
-const lastModified = document.getElementById("lastModified");
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
 const menu = document.getElementById("menu");
-const navigation = document.querySelector("ul");
-
-let year = new Date().getFullYear();
-currentYear.innerText = year;
-
-let modified = new Date(document.lastModified);
-lastModified.innerText = modified;
+const navigation = document.querySelector("nav ul");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
